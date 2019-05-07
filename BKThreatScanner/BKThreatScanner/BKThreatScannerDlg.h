10fde4ccd5df682b8738bfbef86f0532f4050000
anhnh121 <narala1610@gmail.com>
2019-05-07 11:44:32 +0700

    Blank Project

---
+++ BKThreatScanner/BKThreatScanner/BKThreatScannerDlg.h
@@ -0,0 +1,32 @@
+
+// BKThreatScannerDlg.h : header file
+//
+
+#pragma once
+
+
+// CBKThreatScannerDlg dialog
+class CBKThreatScannerDlg : public CDialogEx
+{
+// Construction
+public:
+	CBKThreatScannerDlg(CWnd* pParent = NULL);	// standard constructor
+
+// Dialog Data
+	enum { IDD = IDD_BKTHREATSCANNER_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
